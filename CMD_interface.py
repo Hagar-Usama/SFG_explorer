ec20aee2bf78d12e8c8d2034956b01ba51d73b34
Hagar Usama <Hagar_Usama@outlook.com>
2018-09-12 01:28:36 +0200

    An simple interface

---
+++ CMD_interface.py
@@ -0,0 +1,10 @@
+import cycle_fun
+import networkx as nx
+
+print("Welcome to SFG Slover ... CMD Version :)")
+print("We will solve the prolem step by step, using Mason Formula to get everything clear")
+
+
+G = nx.DiGraph()
+
+print('\n***Kindly add nodes of the graph in one  row ****')
